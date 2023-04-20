651ea5967bdb8f7b1a0bece59cfc6d13b63efcc8
Alzelida Kryeziu <alzelide.k@gmail.com>
2023-04-20 17:54:16 +0200

    middleware added

--- routers/users.js
+++ routers/users.js
@@ -2,6 +2,10 @@ const router = require('express').Router()
 
 const{getUsers} = require('../controllers/users')
 
+const validateData = require('../middlewares/validateData')
+
 router.get('/', getUsers);
+
+router.get('/', validateData, getUsers)
 
 module.exports = router
